404f0bf085e60a542b07a1a076260707fc61b366
Minh Tu Nguyen Vu <nguyenvuminhtu@vn.oro.com>
2014-12-30 16:22:36 +0700

    add own jQuery plugin

---
+++ j05.getUrlParams/js/getUrlParams.js
@@ -0,0 +1,13 @@
+;(function($) {
+  
+  "use strict";
+  $(function(){
+
+  })
+
+  var getUrlVar = function () {
+    var vars = [], hash;
+    var hashes  = windown.location.href.slice(window.location.href.indexOf('?')+1).split('&');
+  };
+  
+})(jQuery);
